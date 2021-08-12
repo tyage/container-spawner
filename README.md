809d7d6eebb368fe511eac354307e275702c472c
tyage <namatyage@gmail.com>
2021-08-12 12:54:30 +0900

    support recaptcha

--- README.md
+++ README.md
@@ -13,14 +13,16 @@ You can update configuration with environments
 
 |Env|Description|Default Value|
 |-|-|-|
-|SPAWNER_IMAGE_NAME|Name of the image you want to run.||
-|SPAWNER_CONTAINER_PORT|Container port you want to expose. Random host port will be assigned to each containers.||
+|SPAWNER_IMAGE_NAME (*required*)|Name of the image you want to run.||
+|SPAWNER_CONTAINER_PORT (*required*)|Container port you want to expose. Random host port will be assigned to each containers.||
 |SPAENER_HOSTNAME|Hostname of the URL shown to user.|localhost|
 |SPAWNER_USERNAME_ENV|Container receives the random username with this environment.|CS_USERNAME|
 |SPAWNER_PASSWORD_ENV|Container receives the random password with this environment.|CS_PASSWORD|
 |SPAWNER_PORT_MIN|Min random port.|62000|
 |SPAWNER_PORT_MAX|Max random port.|65000|
 |SPAWNER_TIME_LIMIT|Life time of the spawned container.|900|
+|RECAPTCHA_PUBLIC_KEY|Recaptcha site key.||
+|RECAPTCHA_PRIVATE_KEY|Recaptcha secret key.||
 
 ## Cleaner App
 
@@ -34,5 +36,5 @@ You can update configuration with environments
 
 |Env|Description|Default Value|
 |-|-|-|
-|SPAWNER_IMAGE_NAME|Name of the image you want to stop after the time limit.||
+|SPAWNER_IMAGE_NAME (*required*)|Name of the image you want to stop after the time limit.||
 |SPAWNER_TIME_LIMIT|Life time of the spawned container.|900|
--- spawner/app.py
+++ spawner/app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm, RecaptchaField
 import docker
 import os
 import random
 import string
 
-app = Flask(__name__)
-client = docker.from_env()
-
+# Environment settings
 IMAGE_NAME = os.environ.get('SPAWNER_IMAGE_NAME')
 if not IMAGE_NAME:
     raise ValueError("No SPAWNER_IMAGE_NAME set")
@@ -19,7 +18,10 @@ SPAWNER_PASSWORD_ENV = os.environ.get('SPAWNER_PASSWORD_ENV', 'CS_PASSWORD')
 PORT_MIN = os.environ.get('SPAWNER_PORT_MIN', 62000)
 PORT_MAX = os.environ.get('SPAWNER_PORT_MAX', 65000)
 TIME_LIMIT = os.environ.get('SPAWNER_TIME_LIMIT', 15 * 60)
+RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
+RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
 
+# Utilities
 def random_port():
     # TODO: exclude used port
     return random.randint(PORT_MIN, PORT_MAX)
@@ -28,13 +30,31 @@ def random_string(length: int = 16):
     letters = string.ascii_letters + string.digits
     return "".join(random.sample(string.ascii_letters, length))
 
+class SpawnForm(FlaskForm):
+    if RECAPTCHA_PUBLIC_KEY and RECAPTCHA_PRIVATE_KEY:
+        recaptcha = RecaptchaField()
+
+# Setup app
+app = Flask(__name__)
+client = docker.from_env()
+
+app.config['SECRET_KEY'] = random_string()
+
+if RECAPTCHA_PUBLIC_KEY and RECAPTCHA_PRIVATE_KEY:
+    app.config['RECAPTCHA_PUBLIC_KEY'] = RECAPTCHA_PUBLIC_KEY
+    app.config['RECAPTCHA_PRIVATE_KEY'] = RECAPTCHA_PRIVATE_KEY
+
 @app.get("/")
 def index():
-    return render_template('index.html')
+    form = SpawnForm()
+    return render_template('index.html', form=form)
 
 @app.post("/")
 def new_instance():
-    # TODO: check recaptcha
+    form = SpawnForm()
+    if not form.validate_on_submit():
+        return "invalid request"
+
     exposed_port = random_port()
     username = random_string()
     password = random_string()
@@ -47,4 +67,4 @@ def new_instance():
             SPAWNER_USERNAME_ENV: username,
             SPAWNER_PASSWORD_ENV: password
         })
-    return render_template('index.html', port=exposed_port, username=username, password=password, host=SPAWNER_HOSTNAME, time_limit=TIME_LIMIT)
+    return render_template('index.html', form=form, port=exposed_port, username=username, password=password, host=SPAWNER_HOSTNAME, time_limit=TIME_LIMIT)
