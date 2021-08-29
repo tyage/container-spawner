d48fd5da04ba6e05358f20d200bc7ef55bbc15c0
tyage <namatyage@gmail.com>
2021-08-29 14:22:45 +0900

    autopep8

--- spawner/app.py
+++ spawner/app.py
@@ -13,14 +13,16 @@ if not IMAGE_NAME:
 CONTAINER_PORT = os.environ.get('SPAWNER_CONTAINER_PORT')
 if not CONTAINER_PORT:
     raise ValueError("No SPAWNER_CONTAINER_PORT set")
-SPAWNER_HOSTNAME = os.environ.get('SPAENER_HOSTNAME', 'localhost') # hostname of a user access URL
+# hostname of a user access URL
+SPAWNER_HOSTNAME = os.environ.get('SPAENER_HOSTNAME', 'localhost')
 SPAWNER_USERNAME_ENV = os.environ.get('SPAWNER_USERNAME_ENV', 'CS_USERNAME')
 SPAWNER_PASSWORD_ENV = os.environ.get('SPAWNER_PASSWORD_ENV', 'CS_PASSWORD')
 PORT_MIN = os.environ.get('SPAWNER_PORT_MIN', 62000)
 PORT_MAX = os.environ.get('SPAWNER_PORT_MAX', 65000)
 TIME_LIMIT = os.environ.get('SPAWNER_TIME_LIMIT', 15 * 60)
-CONTAINER_ARGS = os.environ.get('SPAWNER_CONTAINER_ARGS') # should be JSON string
+CONTAINER_ARGS = os.environ.get('SPAWNER_CONTAINER_ARGS')
 if CONTAINER_ARGS:
+    # should be JSON string
     CONTAINER_ARGS = json.loads(CONTAINER_ARGS)
 else:
     CONTAINER_ARGS = {}
@@ -28,17 +30,22 @@ RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
 RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
 
 # Utilities
+
+
 def random_port():
     # TODO: exclude used port
     return random.randint(PORT_MIN, PORT_MAX)
+
 
 def random_string(length: int = 16):
     letters = string.ascii_letters + string.digits
     return "".join(random.sample(string.ascii_letters, length))
 
+
 class SpawnerForm(FlaskForm):
     if RECAPTCHA_PUBLIC_KEY and RECAPTCHA_PRIVATE_KEY:
         recaptcha = RecaptchaField()
+
 
 # Setup app
 app = Flask(__name__)
@@ -50,10 +57,12 @@ if RECAPTCHA_PUBLIC_KEY and RECAPTCHA_PRIVATE_KEY:
     app.config['RECAPTCHA_PUBLIC_KEY'] = RECAPTCHA_PUBLIC_KEY
     app.config['RECAPTCHA_PRIVATE_KEY'] = RECAPTCHA_PRIVATE_KEY
 
+
 @app.get("/")
 def index():
     form = SpawnerForm()
     return render_template('index.html', form=form)
+
 
 @app.post("/")
 def new_instance():
@@ -79,6 +88,7 @@ def new_instance():
     # TODO: check if container is successfully spawned
 
     return render_template('index.html', form=form, port=exposed_port, username=username, password=password, host=SPAWNER_HOSTNAME, time_limit=TIME_LIMIT)
+
 
 if __name__ == '__main__':
     app.run()
